6771a2a17822a693455a5504ef83bb72e61e4aba
evertonfilho <evertonfilho0513@gmail.com>
2024-02-21 17:55:16 -0300

    Crud Completo

--- obj/Debug/net8.0/SiteEmprestimos.AssemblyInfo.cs
+++ obj/Debug/net8.0/SiteEmprestimos.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SiteEmprestimos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+110b0663de3053a0bf80877edf75b60d23ecc241")]
 [assembly: System.Reflection.AssemblyProductAttribute("SiteEmprestimos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SiteEmprestimos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
